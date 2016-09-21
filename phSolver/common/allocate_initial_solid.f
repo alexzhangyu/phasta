1e721d64db50a79b1c52f998432edf74a6a774a3
Yu Zhang <zhangy41@rpi.edu>
2016-09-21 16:18:23 -0400

    Allocating and intializing solid b arrays. Temporally, it is design for running w/0 read and write solid array into solution.

--- phSolver/common/allocate_initial_solid.f
+++ phSolver/common/allocate_initial_solid.f
@@ -6,13 +6,15 @@ c       use mattype_m
        include "common.h"
 c
        integer:: i_iniSolid
+       integer:: mattyp_s, lcsyst_s, npro_s, ngauss_s
+       integer:: mattyp_sb, lcsyst_sb, npro_sb, ngauss_sb
 c..................
 c.... allocate space for solid arrays 
  
        blocks_loop: do iblk = 1, nelblk
          mattyp_s = lcblk(7,iblk)
-         lcsyst_s = lcblk(3,nelblk)
-         ngauss_s = nint(lcsyst)
+         lcsyst_s = lcblk(3,iblk)
+         ngauss_s = nint(lcsyst_s)
          npro_s = SIZE(mien(iblk)%p,1)
 !for solid block only
          if (mat_eos(mattyp_s,1).eq.ieos_solid_1)then
@@ -33,4 +35,31 @@ c
 c..
       enddo blocks_loop
 c
+c
+c
+c 
+      boundary_ blocks_loop: do iblk = 1, nelblb
+         mattyp_sb = lcblkb(7,iblk)
+         lcsyst_sb = lcblkb(3,iblk)
+         ngauss_sb = nintb(lcsyst_sb)
+         npro_sb = SIZE(mienb(iblk)%p,1)
+!for solid block only
+         if (mat_eos(mattyp_sb,1).eq.ieos_solid_1)then
+           allocate (bdy_b(iblk)%p(npro_sb,ngauss_sb,6))!for solid
+           allocate (bdy_b_dot(iblk)%p(npro_sb,ngauss_sb,6)) !for solid,added
+           allocate (bdy_b_af(iblk)%p(npro_sb,ngauss_sb,6)) !for solid,added
+c......... these arrays need initialization
+           if (i_iniSolid .eq. 1)then
+             bdy_b(iblk)%p    = zero
+             bdy_b_dot(iblk)%p = zero
+             bdy_b(iblk)%p(:,:,1)= one
+             bdy_b(iblk)%p(:,:,2)= one
+             bdy_b(iblk)%p(:,:,3)= one
+             bdy_b_af(iblk)%p = bdy_b(iblk)%p
+           endif
+c
+          endif
+c..
+      enddo boundary_blocks_loop
+
       end
