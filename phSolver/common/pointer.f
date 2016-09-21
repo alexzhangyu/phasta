57959b5443622d0ae401b3dbd729801523cf9b08
Yu Zhang <zhangy41@rpi.edu>
2016-09-21 16:47:16 -0400

    adding data structure/pointer for solid on boundary

--- phSolver/common/pointer.f
+++ phSolver/common/pointer.f
@@ -47,7 +47,11 @@ c
          type (r3d), dimension(MAXBLK2) ::  mBCB
          type (r3d), dimension(MAXBLK2) ::  b !for solid,left Cauchy_green tensor,added
          type (r3d), dimension(MAXBLK2) ::  b_dot!time derivative of b,added
-         type (r3d), dimension(MAXBLK2) ::  b_af! b at time step n+af,added          
+         type (r3d), dimension(MAXBLK2) ::  b_af! b at time step n+af,added
+         type (r3d), dimension(MAXBLK2) ::  bdy_b !left Cauchy_green tensor on the boundary,added
+         type (r3d), dimension(MAXBLK2) ::  bdy_b_dot!time derivative of b on the boudary,added
+         type (r3d), dimension(MAXBLK2) ::  bdy_b_af! b at time step n+af on the boudary,added
+         
 c          
          real*8, allocatable :: gmass(:)
        end module
