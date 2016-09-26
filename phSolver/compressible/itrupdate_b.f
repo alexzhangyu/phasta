db8a9576db0515c22c84ed230f57377b785c4abb
Yu Zhang <zhangy41@rpi.edu>
2016-09-26 11:37:36 -0400

    update the b arrays for interior and boundary blks at the end of each time step

--- phSolver/compressible/itrupdate_b.f
+++ phSolver/compressible/itrupdate_b.f
@@ -7,15 +7,24 @@ c      implicit none
       use mattype_m
       include "common.h"
 c
-      real*8, dimension(npro,ngauss,6) :: b_temp,b_dot_temp
+      real*8, dimension(:,:,:), allocatable :: b_temp,b_dot_temp
+      real*8, dimension(:,:,:), allocatable :: bdy_b_temp, bdy_b_dot_temp
+c      real*8, dimension(npro,ngaussb,6) :: bdy_b_temp,bdy_b_dot_temp
+      integer:: mater_s
+      integer:: mater_sb
+      integer:: npro_temp, npro_b_temp
+
 c    
-      material_loop: do imattype = 1, nummat
-c
+c.....for interior blocks
          blocks_loop: do iblk = 1, nelblk
+           mater_s = lcblk(7,iblk)
 c
 !for solid block only
-           if (mat_eos(imattype,1).eq.ieos_solid_1)then
+           if (mat_eos(mater_s,1).eq.ieos_solid_1)then
 c.....save the temp
+              npro_temp = SIZE(b(iblk)%p,1)
+              allocate(b_temp(npro_temp,ngauss,6))
+              allocate(b_dot_temp(npro_temp,ngauss,6))
               b_temp(:,:,:) = b(iblk)%p(:,:,:)
               b_dot_temp(:,:,:) = b_dot(iblk)%p(:,:,:)
 c
@@ -25,10 +34,34 @@ c.....Do the update
               b_dot(iblk)%p(:,:,:)  =  -one/(alfBi * gamBi * Delt(1))* b_temp(:,:,:)
      &+                           (one - one/gamBi) * b_dot_temp(:,:,:)
      &+                           one/(alfBi * gamBi * Delt(1)) * b_af(iblk)%p(:,:,:)
+              deallocate(b_temp,b_dot_temp)
            endif
 c..
-        enddo blocks_loop
-      enddo material_loop 
+        enddo blocks_loop 
+c
+c.....for boundary blocks
+         boundary_blocks_loop: do iblk = 1, nelblb
+           mater_sb = lcblkb(7,iblk)
+c
+!for solid block only
+           if (mat_eos(mater_sb,1).eq.ieos_solid_1)then
+c.....save the temp
+              npro_b_temp = SIZE(bdy_b(iblk)%p,1)
+              allocate(bdy_b_temp(npro_b_temp,ngaussb,6))
+              allocate(bdy_b_dot_temp(npro_b_temp,ngaussb,6)) 
+              bdy_b_temp(:,:,:) = bdy_b(iblk)%p(:,:,:)
+              bdy_b_dot_temp(:,:,:) = bdy_b_dot(iblk)%p(:,:,:)
+c
+c.....Do the update
+              bdy_b(iblk)%p(:,:,:) = (one/alfBi) * (bdy_b_af(iblk)%p(:,:,:) - bdy_b_temp(:,:,:) )
+     &+                          bdy_b_temp(:,:,:)
+              bdy_b_dot(iblk)%p(:,:,:)  =  -one/(alfBi * gamBi * Delt(1))* bdy_b_temp(:,:,:)
+     &+                           (one - one/gamBi) * bdy_b_dot_temp(:,:,:)
+     &+                           one/(alfBi * gamBi * Delt(1)) * bdy_b_af(iblk)%p(:,:,:)
+              deallocate(bdy_b_temp, bdy_b_dot_temp)
+            endif
+c..
+        enddo boundary_blocks_loop 
 c
       return
       end subroutine itrupdate_b
