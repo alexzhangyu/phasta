fad39a4449ffdb6572e93f0fb0e328c1c232cf52
Yu Zhang <zhangy41@rpi.edu>
2016-09-29 10:11:39 -0400

    adding the subroutine to calculate the element-wise solid array

--- phSolver/compressible/itrupdate_b.f
+++ phSolver/compressible/itrupdate_b.f
@@ -1,4 +1,4 @@
-      subroutine itrupdate_b
+      subroutine itrupdate_b(elmb1,elmb2)
 c....Update the left Cauchy-green tensor at the end of each time step
 c       
       
@@ -13,6 +13,10 @@ c      real*8, dimension(npro,ngaussb,6) :: bdy_b_temp,bdy_b_dot_temp
       integer:: mater_s
       integer:: mater_sb
       integer:: npro_temp, npro_b_temp
+c
+      real*8 elmb1(numel,3) !store elm-wise solid array
+      real*8 elmb2(numel,3) !store elm-wise solid array
+      integer::iel_temp, lcsyst_temp, ngauss_temp
 
 c    
 c.....for interior blocks
@@ -23,6 +27,10 @@ c
            if (mat_eos(mater_s,1).eq.ieos_solid_1)then
 c.....save the temp
               npro_temp = SIZE(b(iblk)%p,1)
+              iel_temp = lcblk(1,iblk)
+              lcsyst_temp = lcblk(3,iblk)
+              ngauss_temp = nint(lcsyst_temp)
+c
               allocate(b_temp(npro_temp,ngauss,6))
               allocate(b_dot_temp(npro_temp,ngauss,6))
               b_temp(:,:,:) = b(iblk)%p(:,:,:)
@@ -35,6 +43,13 @@ c.....Do the update
      &+                           (one - one/gamBi) * b_dot_temp(:,:,:)
      &+                           one/(alfBi * gamBi * Delt(1)) * b_af(iblk)%p(:,:,:)
               deallocate(b_temp,b_dot_temp)
+c
+c.....fill the elm-wise solid arrays
+              call fillelmb( elmb1, elmb2, iel_temp,    npro_temp,
+     &                       lcsyst_temp,  ngauss_temp, b(iblk)%p )
+c.....end of fill elm-wise solid arrays
+c
+c.....end of updates
            endif
 c..
         enddo blocks_loop 
@@ -59,6 +74,7 @@ c.....Do the update
      &+                           (one - one/gamBi) * bdy_b_dot_temp(:,:,:)
      &+                           one/(alfBi * gamBi * Delt(1)) * bdy_b_af(iblk)%p(:,:,:)
               deallocate(bdy_b_temp, bdy_b_dot_temp)
+c.....end of updates
             endif
 c..
         enddo boundary_blocks_loop 
