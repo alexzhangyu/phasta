178ccea845b763a5ee52834ba6717709cb892f3b
Yu Zhang <zhangy41@rpi.edu>
2016-11-09 11:53:23 -0500

    ignore some calculation in e3ivar_solid soubroutine due to the change of interface at e3 level(HEAD^2 COMMIT)

--- phSolver/compressible/e3ivar_solid.f
+++ phSolver/compressible/e3ivar_solid.f
@@ -4,30 +4,24 @@
         real*8, dimension(npro,nflow),target, intent(in) :: g1yi_,g2yi_,g3yi_
         real*8, intent(in) :: almBi_, alfBi_, gamBi_
 c
-        almBi = almBi_
-        alfBi = alfBi_
-        gamBi = gamBi_  
+c        almBi = almBi_
+c        alfBi = alfBi_
+c        gamBi = gamBi_  
+cc
+c        allocate(dudx(npro,3))
+c        allocate(dudy(npro,3))
+c        allocate(dudz(npro,3))
 c
-c        dudx => g1yi_(:,2:4)
-c        dudy => g2yi_(:,2:4)
-c        dudz => g3yi_(:,2:4)
-        allocate(dudx(npro,3))
-        allocate(dudy(npro,3))
-        allocate(dudz(npro,3))
+c        dudx = g1yi_(:,2:4)
+c        dudy = g2yi_(:,2:4)
+c        dudz = g3yi_(:,2:4)
 c
-        dudx = g1yi_(:,2:4)
-        dudy = g2yi_(:,2:4)
-        dudz = g3yi_(:,2:4)
+c        call calc_solid
+cc       
+c        deallocate(dudx)
+c        deallocate(dudy)
+c        deallocate(dudz)
 
-c
-        call calc_solid
-c
-        deallocate(dudx)
-        deallocate(dudy)
-        deallocate(dudz)
-c
-
-c
       end subroutine e3ivar_solid
 c
       subroutine e3bvar_solid(g1yb_,g2yb_,g3yb_,almBi_,alfBi_,gamBi_)
@@ -36,25 +30,25 @@ c
         real*8, dimension(npro,nflow),target, intent(in) :: g1yb_,g2yb_,g3yb_
         real*8, intent(in) :: almBi_, alfBi_, gamBi_
 c
-        almBi = almBi_
-        alfBi = alfBi_
-        gamBi = gamBi_  
-c
-c        dudx => g1yi_(:,2:4)
-c        dudy => g2yi_(:,2:4)
-c        dudz => g3yi_(:,2:4)
-        allocate(dudx(npro,3))
-        allocate(dudy(npro,3))
-        allocate(dudz(npro,3))
-c
-        dudx = g1yb_(:,2:4)
-        dudy = g2yb_(:,2:4)
-        dudz = g3yb_(:,2:4)
-c
-        call calc_solid_bdy
-c
-        deallocate(dudx)
-        deallocate(dudy)
-        deallocate(dudz)
+c        almBi = almBi_
+c        alfBi = alfBi_
+c        gamBi = gamBi_  
+cc
+cc        dudx => g1yi_(:,2:4)
+cc        dudy => g2yi_(:,2:4)
+cc        dudz => g3yi_(:,2:4)
+c        allocate(dudx(npro,3))
+c        allocate(dudy(npro,3))
+c        allocate(dudz(npro,3))
+cc
+c        dudx = g1yb_(:,2:4)
+c        dudy = g2yb_(:,2:4)
+c        dudz = g3yb_(:,2:4)
+cc
+c        call calc_solid_bdy
+cc
+c        deallocate(dudx)
+c        deallocate(dudy)
+c        deallocate(dudz)
 c
       end subroutine e3bvar_solid
